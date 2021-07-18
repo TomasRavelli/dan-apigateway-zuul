7648b0109c09acbb7db17a4d0992cd6fb59f84f5
Tomas Ravelli <tomy_ravelli@hotmail.com>
2021-07-18 10:48:51 -0300

    Primer Commit

---
+++ src/main/java/dan/tp2021/zuul/DanApigatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package dan.tp2021.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanApigatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanApigatewayZuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dan/tp2021/zuul/DanApigatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.tp2021.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanApigatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
