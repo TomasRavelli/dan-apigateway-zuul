dba29103081a6d95c3406bf4f7ecebf2aa7e78f0
Gaston <gaston.diaz.contractor@macmillan.com>
2021-07-27 19:29:45 -0300

    Fix test again
    La clase tiene que ser publica

--- src/test/java/dan/tp2021/zuul/DanApigatewayZuulApplicationTests.java
+++ src/test/java/dan/tp2021/zuul/DanApigatewayZuulApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DanApigatewayZuulApplicationTests {
+public class DanApigatewayZuulApplicationTests {
 
 	@Test
 	public void contextLoads() {
